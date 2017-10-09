504de2632be52fb9d372849d4cf2bd162617bbb7
Anay Nayak <anayak007@gmail.com>
2017-10-09 22:19:48 +0530

    Adding search, event_type highlight

--- js/panel.js
+++ js/panel.js
@@ -1,11 +1,13 @@
 var events = $('#events');
+var contentElement = $(".context");
+var keywordElement = $("#keyword");
 
 function processRequest(data) {
     var event = $.deparam(decodeURIComponent(data));
     var jsonEle = renderjson.set_icons('+', '-')
         .set_show_to_level("all")(JSON.parse(decodeURIComponent(event.e)));
-
-    document.getElementById("events").appendChild(jsonEle);
+    events.append(jsonEle);
+    $(jsonEle).find("span.key:contains('event_type')").addClass('event_type');
 }
 
 function clearView() {
@@ -28,3 +30,28 @@ if (chrome.devtools) {
         processRequest("e=" + encodeURIComponent(JSON.stringify(data)))
     })
 }
+
+function jumpTo(element) {
+    var position = element.offset().top - 50;
+    window.scrollTo(0, position);
+}
+
+var mark = function () {
+    var key = keywordElement.val();
+    var options = {
+        wildcards: "enabled",
+        debug: true,
+        diacritics: false,
+        done: function () {
+            jumpTo(contentElement.find("mark"));
+        }
+    };
+    contentElement.unmark({
+        done: function () {
+            contentElement.mark(key, options);
+
+        }
+    });
+};
+
+keywordElement.on("input", mark);
